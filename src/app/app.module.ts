11e6ae918c94842dc0c348c753044a2c6c1f3a01
christhao26 <79154880+christhao26@users.noreply.github.com>
2021-04-02 14:16:14 -0400

    intial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,26 @@ import { FormsModule } from '@angular/forms';
 // import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StudentsComponent } from './students/students.component';
+import { StudentDetailComponent } from './student-detail/student-detail.component';
+import { MessagesComponent } from './messages/messages.component';
+import { AppRoutingModule } from './app-routing.module';
+import { HomepageComponent } from './homepage/homepage.component';
+// import { PlanetsComponent } from './planets/planets.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    StudentsComponent
+    StudentsComponent,
+    StudentDetailComponent,
+    MessagesComponent,
+    HomepageComponent,
+    // PlanetsComponent
   ],
   imports: [
     BrowserModule,
     // AppRoutingModule,
-    FormsModule
+    FormsModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
